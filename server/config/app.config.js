cd93a0c0a548fc6a9fa113ab237235882291c325
alex <alexalvarez@mail.com>
2021-06-26 09:25:14 +0100

    Update router

--- server/config/app.config.js
+++ server/config/app.config.js
@@ -3,13 +3,10 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { join } = require('path');
+// const { join } = require('path');
 const bodyParser = require('body-parser');
 
-const articlesRoutes = require(join(__dirname, '../routes/article.router'));
-const membersRoutes = require(join(__dirname, '../routes/member.router'));
-const adminRoutes = require(join(__dirname, '../routes/admin.router'));
-const authRoutes = require(join(__dirname, '../routes/auth.router'));
+// const apiRouter = require(join(__dirname, '../routes/api.router'));
 
 const app = express();
 
@@ -20,9 +17,10 @@ app.use(morgan('tiny'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(helmet());
-app.use('/api/articles', articlesRoutes);
-app.use('/api/members', membersRoutes);
-app.use('/admin', adminRoutes);
-app.use('/auth', authRoutes);
+// app.use('/api', apiRouter);
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
 
 module.exports = app;
