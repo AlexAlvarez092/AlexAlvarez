125c2bc7b06e356943c41e7b9be979657150a8b2
alex <alexalvarez@mail.com>
2021-06-26 09:25:37 +0100

    Use https

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 /* eslint-disable import/no-dynamic-require */
 const { join } = require('path');
+const { readFileSync } = require('fs');
+const https = require('https');
 
 require('dotenv').config();
 
 const app = require(join(__dirname, './config/app.config'));
 
-require(join(__dirname, './config/db.config'));
+// require(join(__dirname, './config/db.config'));
 
-app.listen(app.get('port'), () => {
-  console.log(`HTTP server running on port ${app.get('port')}`);
+const httpsServer = https.createServer({
+  key: readFileSync(join(__dirname, './ssl/privKey.pem')),
+  cert: readFileSync(join(__dirname, './ssl/fullchain.pem')),
+}, app);
+
+httpsServer.listen(app.get('port'), () => {
+  console.log('HTTPS server running on port', app.get('port'));
 });
