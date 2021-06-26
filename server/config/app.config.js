be6fc8db96660050c4e77e3f47d39df2654698e9
alex <alexalvarez@mail.com>
2021-06-26 09:45:06 +0100

    Ready to prod

--- server/config/app.config.js
+++ server/config/app.config.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-// const { join } = require('path');
+const { join } = require('path');
 const bodyParser = require('body-parser');
 
 // const apiRouter = require(join(__dirname, '../routes/api.router'));
@@ -17,10 +17,11 @@ app.use(morgan('tiny'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(helmet());
+app.use(express.static(join(__dirname, '../public')));
 // app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.sendFile(join(__dirname, '../views/index.html'));
 });
 
 module.exports = app;
